e6322f78ee20f92bc2ae2d4bd9cdcb79f36252a8
cpf2021-gif <2992247892@qq.com>
2023-05-23 23:13:28 +0800

    test(database): Add database connection test
    This commit adds a test case to verify the successful connection to the
    databae using Gorm.

--- main.go
+++ main.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	// 配置文件
 	global.GL_VIPER = setup.InitializeViper("./")
+
+	// 连接数据库
+	global.GL_DB = setup.InitializeDB()
 }
---
+++ test/setupDB_test.go
@@ -0,0 +1,12 @@
+package test
+
+import (
+	"testing"
+
+	"server/setup"
+)
+
+func TestInitializeDB(t *testing.T) {
+	setup.InitializeViper(".././")
+	setup.InitializeDB()	
+}
