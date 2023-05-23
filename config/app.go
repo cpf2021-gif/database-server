eebabc292f6dd945ce1500f079c78234f3cacdc6
cpf2021-gif <2992247892@qq.com>
2023-05-23 21:24:28 +0800

    feat(config): Add configuration struct
    This commit introduces a new structure to store application
    configuration settings.

---
+++ config/app.go
@@ -0,0 +1,7 @@
+package config
+
+type App struct {
+	Name string `mapstructure:"name" json:"name" yaml:"name"`
+	DBtype string `mapstructure:"dbtype" json:"dbtype" yaml:"dbtype"`
+	Port int `mapstructure:"port" json:"port" yaml:"port"`
+}
