ca5a317ea3483fa4c9fff8718eec8e174cc0998a
cpf2021-gif <2992247892@qq.com>
2023-05-24 15:51:49 +0800

    test(password): Add test cases for password handling functions
    This commit includes test cases to verify the correctness of the
    password handling functions.

---
+++ test/password_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"server/util"
+	"testing"
+
+)
+
+func TestPassword(t *testing.T) {
+	password := "123456"
+	hashPassword, err := util.HashAndSalt(password)
+	if err != nil {
+		t.Error(err)
+	}
+	isValid := util.CheckPasswordHash(password, hashPassword)
+	if !isValid {
+		t.Error("Password is not valid")
+	}	
+}
