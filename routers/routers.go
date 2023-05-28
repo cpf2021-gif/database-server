98bccf8e8637a897ec2e7c2903c9ecfdad75c354
cpf2021-gif <2992247892@qq.com>
2023-05-28 22:52:24 +0800

    feat(cors): Add CORS middleware for cross-origin requests

--- routers/routers.go
+++ routers/routers.go
@@ -12,11 +12,30 @@ import (
 	"server/util"
 )
 
+// 定义跨域中间件
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, PATCH")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		c.Next()
+	}
+}
+
 func InitRouter() *gin.Engine {
 	r := gin.Default()
 
 	// 中间件
-
+	r.Use(Cors())
 	// 路由
 	// 备份数据库
 	r.GET("/backup", func(c *gin.Context) {
